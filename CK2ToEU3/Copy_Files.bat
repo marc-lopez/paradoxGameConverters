3fd23776ba83000f980225c3b5872d594b9864cc
idhrendur <devnull@localhost>
2013-12-27 12:57:23 -0800

    Automate changelog

--- CK2ToEU3/Copy_Files.bat
+++ CK2ToEU3/Copy_Files.bat
@@ -1,9 +1,12 @@
 copy "Data Files\blocked_nations.txt" "release\blocked_nations.txt"
-copy "Data Files\changelog.txt" "release\changelog.txt"
 copy "Data Files\configuration.txt" "release\configuration.txt"
 copy "Data Files\country_mappings.txt" "release\country_mappings.txt"
 copy "Data Files\culture_mappings.txt" "release\culture_mappings.txt"
 copy "Data Files\religion_mappings.txt" "release\religion_mappings.txt"
 copy "Data Files\province_mappings.txt" "release\province_mappings.txt"
 copy "Data Files\readme.txt" "release\readme.txt"
-xcopy /E /Y "Converter Mod" "Release"
+xcopy /E /Y /Q /I "Converter Mod" "Release"
+
+hg log --template "Change:\t\t{rev}: {node}\nAuthor:\t\t{author}\nDescription:\t{desc}\nDate:\t\t{date|isodate}\nBranch:\t\t{branch}\n***\n" > Release/log.txt
+(for /f "delims=" %%i in (release/log.txt) do @echo %%i)>release/changelog.txt
+del release\log.txt
