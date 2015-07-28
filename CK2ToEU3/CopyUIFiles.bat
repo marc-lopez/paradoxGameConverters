194e74613958e5d7332957d272d62f8d5678b206
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-07-28 19:50:41 -0400

    Delete UI Folder

--- CK2ToEU3/CopyUIFiles.bat
+++
@@ -1,7 +0,0 @@
-copy "Converter.UI.exe" "%1\Converter.UI.exe"
-copy "UserPreferences.xml" "%1\UserPreferences.xml"
-copy "Configuration.xml" "%1\Configuration.xml"
-
-del Converter.UI.exe
-del UserPreferences.xml
-del Configuration.xml
